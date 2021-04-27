84b78fa8ef32246fc4022095888501a2a8d306b1
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2021-04-27 10:39:45 -0700

    updating models

--- src/components/gocam-selector/gocam-selector.tsx
+++ src/components/gocam-selector/gocam-selector.tsx
@@ -18,6 +18,29 @@ export class GoCamSelector {
     list = [
         "",
         "http://model.geneontology.org/568b0f9600000284",
+
+        // MF - causal - MF - causal - MF (Apr 2021)
+        "http://model.geneontology.org/5fa76ad400000265",
+        "http://model.geneontology.org/5fa76ad400000110",
+        "http://model.geneontology.org/5fa76ad400000018",
+        "http://model.geneontology.org/5fadbcf000002236",
+        "http://model.geneontology.org/5fadbcf000002042",
+        "http://model.geneontology.org/5fce9b7300000367",
+        "http://model.geneontology.org/5fa76ad400000374",
+        "http://model.geneontology.org/5fce9b7300000434",
+        "http://model.geneontology.org/5fadbcf000000942",
+        "http://model.geneontology.org/5fadbcf000000868",
+        "http://model.geneontology.org/5fadbcf000000480",
+        "http://model.geneontology.org/5f46c3b700003884",
+        "http://model.geneontology.org/5f46c3b700003802",
+        "http://model.geneontology.org/5d29221b00002847",
+        "http://model.geneontology.org/5df932e000002650",
+        "http://model.geneontology.org/5b91dbd100002057",
+        "http://model.geneontology.org/5ce58dde00001215",
+        "http://model.geneontology.org/59dc728000000555",
+        "http://model.geneontology.org/57c82fad00000695",
+        
+        // Other MF - causal - MF
         "http://model.geneontology.org/5e72450500003881",
         "http://model.geneontology.org/5c4605cc00003261",
         "http://model.geneontology.org/5b91dbd100001993",
@@ -32,24 +55,19 @@ export class GoCamSelector {
         "http://model.geneontology.org/5e72450500001046",
         "http://model.geneontology.org/5f46c3b700000011",
         "http://model.geneontology.org/5e72450500000869",
-        "http://model.geneontology.org/5b91dbd100002057",
-        "http://model.geneontology.org/5ce58dde00001215",
         "http://model.geneontology.org/57c82fad00000784",
         "http://model.geneontology.org/5ce58dde00001327",
         "http://model.geneontology.org/5716c41300000422",
         "http://model.geneontology.org/5e72450500000000",
-        "http://model.geneontology.org/5df932e000002650",
         "http://model.geneontology.org/5d29221b00001995",
         "http://model.geneontology.org/5b318d0900000302",
         "http://model.geneontology.org/5df932e000003298",
         "http://model.geneontology.org/5d29221b00001945",
         "http://model.geneontology.org/5c4605cc00004193",
         "http://model.geneontology.org/5ae3b0f600001522",
-        "http://model.geneontology.org/5d29221b00002847",
         "http://model.geneontology.org/5ed1aab700000290",
         "http://model.geneontology.org/5df932e000002440",
         "http://model.geneontology.org/5db9c9a500000632",
-        "http://model.geneontology.org/57c82fad00000695",
         "http://model.geneontology.org/57c82fad00000403",
         "http://model.geneontology.org/5716c41300000389",
         "http://model.geneontology.org/5716c41300000277",
@@ -57,8 +75,7 @@ export class GoCamSelector {
         "http://model.geneontology.org/5d29221b00002807",
         "http://model.geneontology.org/5d29221b00001607",
         "http://model.geneontology.org/580e952200000046",
-        "http://model.geneontology.org/59bee34700000179",
-        "http://model.geneontology.org/5f46c3b700003802"
+        "http://model.geneontology.org/59bee34700000179"
     ]
 
     componentWillLoad() {

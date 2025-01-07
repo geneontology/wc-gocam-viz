5467502e377b304cc0d8e8bb9ba05626a0a492bb
Patrick Kalita <pkalita@lbl.gov>
2025-01-07 10:11:31 -0800

    Use inline SVG instead of asset file for evidence icon

--- src/components/genes-panel/genes-panel.tsx
+++ src/components/genes-panel/genes-panel.tsx
@@ -2,8 +2,6 @@ import { Component, Prop, Element, Event, EventEmitter, Watch, h, Method } from 
 import { State } from '@stencil/core';
 import { Activity, ActivityNode, Cam, Evidence, noctuaFormConfig } from '../../globals/@noctua.form';
 
-import newspaperPNG from './assets/newspaper.png';
-
 /**
  * @part process - Process containers
  * @part activity - Activity containers
@@ -104,6 +102,18 @@ export class GenesPanel {
         }
     }
 
+    renderReferenceIcon() {
+        // SVG originally from https://icons.getbootstrap.com/icons/newspaper/
+        // Modified to remove the width and height attributes in order to allow the size to be
+        // controlled via CSS
+        return (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-newspaper icon" viewBox="0 0 16 16">
+              <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z"/>
+              <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z"/>
+          </svg>
+        )
+    }
+
     /**
      * Render the references for a biological context
      * @param context a biological context (see graphHandler)
@@ -115,7 +125,7 @@ export class GenesPanel {
                     evidences.map(evidence => {
                         return <a href={evidence.referenceEntity?.url} target='_blank'
                             title={"Source: " + evidence.reference + "\nEvidence: " + evidence.evidence.label}>
-                            <img class="icon" src={newspaperPNG} alt="newspaper" />
+                            { this.renderReferenceIcon() }
                         </a>
                     })
                 }
